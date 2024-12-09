c08c0678bf57ca84c31d01403cafe069beb371cb
Sarah Reichelt <sarah@troz.net>
2024-12-09 14:26:09 +1000

    Initial Commit

---
+++ Xcode_Config/SubView.swift
@@ -0,0 +1,13 @@
+// Created on 9/12/2024.
+
+import SwiftUI
+
+struct SubView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubView()
+}
---
+++ Xcode_Config/Xcode_ConfigApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_ConfigApp.swift
+//  Xcode_Config
+//
+//  Created by Sarah Reichelt on 9/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_ConfigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
