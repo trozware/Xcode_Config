62ceb8d7117cbfb91dc867546cdfb1e10e8b0d1d
Sarah Reichelt <sarah@troz.net>
2024-12-09 14:53:18 +1000

    add a useless button

--- Xcode_Config/ContentView.swift
+++ Xcode_Config/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
         .imageScale(.large)
         .foregroundStyle(.tint)
       Text("Hello, Git!")
+      
+      Button("Click Here") {
+        // add action later...
+      }
     }
     .padding()
   }
