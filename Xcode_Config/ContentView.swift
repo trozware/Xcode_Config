b1c708d51d62dbaffd9c68c423ab5fd23e32a5b1
Sarah Two <sarah@troz.net>
2024-12-10 11:09:45 +1000

    make text large and red

--- Xcode_Config/ContentView.swift
+++ Xcode_Config/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
         .imageScale(.large)
         .foregroundStyle(.tint)
       Text("Hello, Git!")
+        .font(.title)
+        .foregroundStyle(.red)
       
       Button("Click Here") {
         // add action later...
